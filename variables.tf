e4b65f375d2750b3500ac4891537e820761f1702
moniskhantelus <93679325+moniskhantelus@users.noreply.github.com>
2024-07-03 17:17:25 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,96 +1,33 @@
-variable "project_id" {
-  description = "Project id of the project that holds the network."
-  type        = string
-}
-
-variable "network_name" {
-  description = "Name of the network this set of firewall rules applies to."
-  type        = string
-}
-
 variable "rules" {
-  description = "This is DEPRICATED and available for backward compatiblity. Use ingress_rules and egress_rules variables. List of custom rule definitions"
-  default     = []
-  type = list(object({
+  description = "list of firewall rules"
+  type = set(object({
     name                    = string
-    description             = optional(string, null)
-    direction               = optional(string, "INGRESS")
-    disabled                = optional(bool, null)
-    priority                = optional(number, null)
-    ranges                  = optional(list(string), [])
-    source_tags             = optional(list(string))
-    source_service_accounts = optional(list(string))
-    target_tags             = optional(list(string))
-    target_service_accounts = optional(list(string))
-
-    allow = optional(list(object({
-      protocol = string
-      ports    = optional(list(string))
-    })), [])
-    deny = optional(list(object({
-      protocol = string
-      ports    = optional(list(string))
-    })), [])
-    log_config = optional(object({
-      metadata = string
-    }))
+    direction               = string
+    protocol                = string
+    priority                = number
+    ports                   = list(string)
+    source_ranges           = list(string)
+    destination_ranges      = list(string)
+    source_tags             = list(string)
+    target_tags             = list(string)
+    use_service_accounts    = bool
+    source_service_accounts = list(string)
+    target_service_accounts = list(string)
   }))
 }
 
-variable "ingress_rules" {
-  description = "List of ingress rules. This will be ignored if variable 'rules' is non-empty"
-  default     = []
-  type = list(object({
-    name                    = string
-    description             = optional(string, null)
-    disabled                = optional(bool, null)
-    priority                = optional(number, null)
-    destination_ranges      = optional(list(string), [])
-    source_ranges           = optional(list(string), [])
-    source_tags             = optional(list(string))
-    source_service_accounts = optional(list(string))
-    target_tags             = optional(list(string))
-    target_service_accounts = optional(list(string))
-
-    allow = optional(list(object({
-      protocol = string
-      ports    = optional(list(string))
-    })), [])
-    deny = optional(list(object({
-      protocol = string
-      ports    = optional(list(string))
-    })), [])
-    log_config = optional(object({
-      metadata = string
-    }))
-  }))
+variable "network" {
+  description = "name of network"
+  type        = string
 }
 
-variable "egress_rules" {
-  description = "List of egress rules. This will be ignored if variable 'rules' is non-empty"
-  default     = []
-  type = list(object({
-    name                    = string
-    description             = optional(string, null)
-    disabled                = optional(bool, null)
-    priority                = optional(number, null)
-    destination_ranges      = optional(list(string), [])
-    source_ranges           = optional(list(string), [])
-    source_tags             = optional(list(string))
-    source_service_accounts = optional(list(string))
-    target_tags             = optional(list(string))
-    target_service_accounts = optional(list(string))
+variable "project_id" {
+  description = "project id"
+  type        = string
+}
 
-    allow = optional(list(object({
-      protocol = string
-      ports    = optional(list(string))
-    })), [])
-    deny = optional(list(object({
-      protocol = string
-      ports    = optional(list(string))
-    })), [])
-    log_config = optional(object({
-      metadata = string
-    }))
-  }))
+variable "log_config_metadata" {
+  description = "Supports `INCLUDE_ALL_METADATA` or `EXCLUDE_ALL_METADATA` If not provided will default to `INCLUDE_ALL_METADATA`"
+  type        = string
+  default     = "INCLUDE_ALL_METADATA"
 }
