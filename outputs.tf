d297c14ce8caf2dd32f72c5fe2294f009dff9c4f
moniskhantelus <93679325+moniskhantelus@users.noreply.github.com>
2024-07-03 17:16:44 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,6 @@
-output "firewall_rules" {
-  value       = google_compute_firewall.rules
-  description = "The created firewall rule resources"
-}
-
-output "firewall_rules_ingress_egress" {
-  value       = google_compute_firewall.rules_ingress_egress
-  description = "The created firewall ingress/egress rule resources"
+output "self_links" {
+  value = {
+    for rule in google_compute_firewall.rules :
+    rule.name => rule.self_link
+  }
 }
